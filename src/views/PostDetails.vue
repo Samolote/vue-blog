46ea35e669684d203fa8de3e49028bd343e23548
Wojciech Łukowski <wojtek304@gmail.com>
2021-05-13 21:11:59 +0200

    Add template for PostDetails view

--- src/views/PostDetails.vue
+++ src/views/PostDetails.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>post details go here</div>
+  <v-container grid-list-xs>
+    <v-card>
+      <v-card-title primary-title>
+        <h3 class="text-h5">
+          Post id {{ this.$route.params.id }}
+        </h3>
+      </v-card-title>
+      <v-card-text>
+        <p class="mb-0">
+          body
+        </p>
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-btn color="primary" depressed>Edit</v-btn>
+        <v-btn color="error" depressed>Delete</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  name: 'PostDetails',
 };
 </script>
