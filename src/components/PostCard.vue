854908649a7dc8e129c700b836f296cad452e32a
Wojciech Łukowski <wojtek304@gmail.com>
2021-05-13 19:40:00 +0200

    Extract v-card from Home to separate component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      <h3 class="text-h5">{{ title.slice(0, 20) }}</h3>
+    </v-card-title>
+    <v-card-text>
+      <p class="mb-0">{{ body ? body.slice(0, 100) : body }}</p>
+    </v-card-text>
+    <v-divider />
+    <v-card-actions>
+      <v-btn text small color="primary">Read more</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+  props: {
+    title: {
+      typeof: String,
+      required: true,
+    },
+    body: {
+      typeof: String,
+      required: false,
+    },
+    id: {
+      typeof: Number,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,12 @@
 <template>
   <v-container grid-list-xs>
     <v-row>
-      <v-col v-for="post in postsChronological" :key="post.id" cols="4">
-        <v-card>
-          <v-card-title primary-title>
-            <h3 class="text-h5">{{ post.title.slice(0, 20) }}</h3>
-          </v-card-title>
-          <v-card-text>
-            <p class="mb-0">{{ post.body.slice(0, 100) }}</p>
-          </v-card-text>
-          <v-divider />
-          <v-card-actions>
-            <v-btn flat color="primary">Read more</v-btn>
-          </v-card-actions>
-        </v-card>
+      <v-col v-for="{title, body, id} in postsChronological" :key="id" cols="4">
+        <PostCard
+          :title="title"
+          :body="body"
+          :id="id"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -22,9 +15,11 @@
 <script>
 // @ is an alias to /src
 import { mapGetters } from 'vuex';
+import PostCard from '@/components/PostCard.vue';
 
 export default {
   name: 'Home',
+  components: { PostCard },
   computed: mapGetters(['postsChronological']),
 };
 </script>
