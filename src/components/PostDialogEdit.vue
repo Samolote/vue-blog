8337cc1ad23a8f0e3bfbcc6dc416ef46fb7c46cc
Wojciech Łukowski <wojtek304@gmail.com>
2021-05-13 22:39:24 +0200

    Add PostDialogEdit component with updatePost hook

---
+++ src/components/PostDialogEdit.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-dialog v-model="dialog" max-width="800px">
+    <template v-slot:activator="{on, attrs}">
+      <v-btn color="primary" depressed v-bind="attrs" v-on="on">
+        <span>Edit</span>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Edit post</span>
+      </v-card-title>
+      <v-form class="mx-5 pb-4" ref="form">
+        <v-text-field label="Title" v-model="post.title" :rules="titleRules"></v-text-field>
+        <v-textarea label="Content" v-model="post.body" :rules="bodyRules"></v-textarea>
+      </v-form>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="success" depressed @click="handleClickConfirm">Confirm</v-btn>
+        <v-btn color="error" depressed @click="handleClickCancel">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'PostDialogEdit',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+      titleRules: [
+        (value) => value.length >= 5 || 'Minimum length is 5 characters.',
+      ],
+      bodyRules: [
+        (value) => value.length >= 50 || 'Minimum length is 50 characters.',
+      ],
+    };
+  },
+  methods: {
+    ...mapActions(['updatePost']),
+    handleClickConfirm() {
+      if (this.$refs.form.validate()) {
+        this.dialog = false;
+        this.updatePost(this.post);
+      }
+    },
+    handleClickCancel() {
+      this.dialog = false;
+    },
+  },
+};
+</script>
